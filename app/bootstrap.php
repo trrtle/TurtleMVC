dacf5941660039b0ad154c0b724218adf3417ca6
Mick <mmaarnoudse@outlook.com>
2019-12-12 10:02:43 +0100

    added comment in boodstrap.php and added the database class, it only creates a connection to the db for now

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,11 +2,7 @@
 // Load config
 require_once 'config/config.php';
 
-// Load libraries
-//require_once 'libs/Controller.php';
-//require_once 'libs/Core.php';
-//require_once 'libs/Database.php';
-
+// load all classes in libs/ when they are instantiated
 spl_autoload_register(function($className){
     require_once "libs/". $className .".php";
 });
--- app/libs/Database.php
+++ app/libs/Database.php
@@ -6,3 +6,32 @@
  * Bind values
  * Return rows and results
  */
+
+class Database{
+    private $db_host = DB_HOST;
+    private $db_user = DB_USER;
+    private $db_secret = DB_SECRET;
+    private $db_name = DB_NAME;
+
+    private $dbh;
+    private $stmt;
+    private $error;
+
+    public function __construct(){
+        // set dsn
+        $dsn = 'mysql:host=' . $this->db_host . ';dbname=' . $this->db_name;
+        $options = [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ];
+
+        // create a PDO instance
+        try{
+            $this->dbh = new PDO($dsn, $this->db_user, $this->db_secret, $options);
+        }catch (PDOException $e){
+            $this->error = $e->getMessage();
+            echo $this->error;
+        }
+    }
+
+}

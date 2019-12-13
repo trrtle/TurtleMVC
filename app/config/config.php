29bba0b958bf323e9329549b1758ce72ba61214c
Mick <mmaarnoudse@outlook.com>
2019-12-13 17:41:12 +0100

    APPVERSION constant added.

--- app/config/config.php
+++ app/config/config.php
@@ -5,9 +5,11 @@ define('DB_USER', '_YOUR_USER_');
 define('DB_SECRET', '_YOUR_PASSWORD_');
 define('DB_NAME', '_YOUR_DB_NAME_');
 
-// app root
-define('APPROOT', dirname(__DIR__));
 // URL root
 define("URLROOT", "__YOUR_URL_)");
 // Site name
 define("SITENAME", "_YOUR_SITE_NAME_");
+// app root
+define('APPROOT', dirname(__DIR__));
+// app version
+define('APPVERSION', "1.0.0");
